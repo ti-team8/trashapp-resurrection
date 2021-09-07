27baf6e885b3de622a5a6529bcaeec9d93e7fb56
Vekt0r11 <vext0r11@gmail.com>
2021-09-06 22:01:22 -0300

    Se agrega ruta /integrantes y Json de integrantes  06/09/2021 22:00

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
  
+//ruta home
 app.get('/home', function (req, res) {
-  res.send('Hello World')
+  res.send('Servidor Express funcionando')
 })
- 
+
+//ruta que muestra el objeto de los integrantes
+app.get('/integrantes', function (req, res) {
+  const json = fs.readFileSync('./JSON/integrantes.json');
+  const integrantes = JSON.parse(json);
+  res.send(integrantes)
+})
+
+//
+
+
 app.listen(3000)
