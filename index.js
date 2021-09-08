28805801d5ce9480b44041d5e3ee5d27e089612c
AyelenINS <ivanainsa2002@gmail.com>
2021-09-08 15:06:21 -0300

    agregar signos

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
  
+//ruta home
 app.get('/home', function (req, res) {
-  res.send('Hello World')
+  res.send('Servidor Express funcionando')
 })
- 
+
+//ruta que muestra el objeto de los integrantes
+app.get('/integrantes', function (req, res) {
+  const json = fs.readFileSync('./JSON/integrantes.json');
+  const integrantes = JSON.parse(json);
+  res.send(integrantes)
+})
+
+//ruta que muestra el objeto de los signos
+app.get('/signos', function (req, res) {
+  const json = fs.readFileSync('./JSON/signos.json');
+  const signos = JSON.parse(json);
+  res.send(signos)
+})
+
+//
+
+
 app.listen(3000)
